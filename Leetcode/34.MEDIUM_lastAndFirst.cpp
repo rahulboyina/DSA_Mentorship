71b349ca761e2b1b867e382f618a2a3b7840b461
Rahul Boyina <rahulboyinaofficial@gmail.com>
2023-09-11 06:31:40 +0530

    code

--- Leetcode/34.MEDIUM_lastAndFirst.cpp
+++ Leetcode/34.MEDIUM_lastAndFirst.cpp
@@ -5,21 +5,3 @@ URL : https://leetcode.com/problems/find-first-and-last-position-of-element-in-s
 
 CODE : 
 
-class Solution {
-public:
-    vector<int> searchRange(vector<int>& nums, int target) {
-        
-        int i=0,j=nums.size()-1;
-        bool f1=true,f2=true;
-        while(i<=j)
-        {
-            if(nums[i]==target){f1=false;}
-            if(nums[j]==target){f2=false;}
-            if((f1==false && f2==false)){return {i,j};}
-            else if(i==j){return {-1,-1};}            
-            if(f1)i++;
-            if(f2)j--;
-        }
-        return {-1,-1};
-    }
-};
