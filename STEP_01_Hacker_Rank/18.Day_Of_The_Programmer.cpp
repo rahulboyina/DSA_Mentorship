a0ef8b8eff7cf82fe9ab17c63a1886f0811788e5
Rahul Boyina <rahulboyinaofficial@gmail.com>
2023-09-20 18:37:10 +0530

    Update 18.Day_Of_The_Programmer.cpp

--- STEP_01_Hacker_Rank/18.Day_Of_The_Programmer.cpp
+++ STEP_01_Hacker_Rank/18.Day_Of_The_Programmer.cpp
@@ -8,3 +8,33 @@
 //CODE
 
 
+
+string dayOfProgrammer(int year) {
+    string str="";
+    if(year==1918){
+    str="26.09."+to_string(year);
+    }
+    else if(year>1918)
+    {
+        if(year%400==0|| (year%4==0 && year%100!=0))
+        {
+            str="12.09."+to_string(year);
+        }
+        else{
+            str="13.09."+to_string(year);
+        }
+    }
+    else{
+            if(year%4==0)
+            {
+                str="12.09."+to_string(year);
+            }
+            else{
+                str="13.09."+to_string(year);
+            }
+        
+        }
+    
+return str;
+
+}
